04d8e7a747f19d167e9f7b673141bd5fab6a3ab4
fadelsaad99 <118170848+fadelsaad99@users.noreply.github.com>
2022-11-15 17:23:00 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
+   float a, b, x, y, z;
+   
+   cout<<"Dati i valori di a, b, x e y"<<endl;
+   cin>>a;
+   cin>>b;
+   cin>>x;
+   cin>>y;
+   if (x < 0 && y > 0)
+   { cout<<z= (a*x – b*y)<<endl;}
+   else if (x ≥ 0 && y ≤ 0)
+   { cout<<z= (a*x*x – b*y)<<endl;}
+   else { cout<<z = (a*x + b*y*y)}
+   
+ 
+   
    return 0;
 }
